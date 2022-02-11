de4b2667fb8827b4e78b42c9eda9aad341a72d40
DillanThomas <mansor.dillan@gmail.com>
2022-02-10 16:41:00 -0800

    theme change

--- public/js/initial.js
+++ public/js/initial.js
@@ -24,8 +24,6 @@ document.querySelectorAll('.title-splash').forEach(element => {
     element.children[0].classList.toggle('w-14')
     element.children[0].classList.toggle('h-96')
     element.children[0].classList.toggle('rotate-180')
-    // element.children[0].children[0].classList.toggle('fill-white')
-    // element.children[0].children[0].classList.toggle('fill-blue-500')
 })
 
 
